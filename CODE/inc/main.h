31d2d8e6d43b46fd7d549a03ff5d34a663897b21
QJD1 <139538576+QJD1@users.noreply.github.com>
2023-07-24 21:02:36 +0800

    2023/7/24 2102

--- CODE/inc/main.h
+++ CODE/inc/main.h
@@ -11,10 +11,16 @@ typedef 	unsigned char	u8;
 typedef 	unsigned int	u16;
 typedef 	unsigned long	u32;
 
+typedef enum Bool
+{
+    false = 0,
+    true  = 1
+};
 
 void SYS_Init();
 void UART1_config();
 void PrintString1(u8 *puts);
+void UART1_Send (u8 bat);
 
 #endif
 
