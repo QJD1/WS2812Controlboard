4dbddeb60df605bd51b488eed4ce5eabb12ce401
QJD1 <139538576+QJD1@users.noreply.github.com>
2023-07-24 20:19:11 +0800

    2023/7/24/2019

---
+++ CODE/inc/main.h
@@ -0,0 +1,20 @@
+#ifndef __MAIN_H_
+#define __MAIN_H_
+
+#include "STC32G.h"
+#include "stdio.h"
+
+#define MAIN_Fosc        22118400L   //定义主时钟（精确计算115200波特率）
+#define Baudrate1   (65536 - MAIN_Fosc / 115200 / 4)
+
+typedef 	unsigned char	u8;
+typedef 	unsigned int	u16;
+typedef 	unsigned long	u32;
+
+
+void SYS_Init();
+void UART1_config();
+void PrintString1(u8 *puts);
+
+#endif
+
